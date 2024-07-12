34f0b561f9cfc38660c6f9118e512fb7b724f9c0
Quang <nguyennhatquangkod2003@gmail.com>
2024-07-12 20:17:35 +0700

    Test socket

---
+++ Assets/__Scripts/Test.cs
@@ -0,0 +1,48 @@
+using Sirenix.OdinInspector;
+using SocketIOClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using TMPro;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+namespace VOU
+{
+    public class Test : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        [Button]
+        async void Socket()
+        {
+            var uri = new Uri("http://localhost:3000");
+            var socket = new SocketIOUnity(uri, new SocketIOOptions
+            {
+                ExtraHeaders = new Dictionary<string, string>
+                {
+                    {"Authorization", "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6MSwiaWF0IjoxNzIwNzg2OTgzLCJleHAiOjE3MjA3OTA1ODN9.lpDU7O-CO14EZWjiAZU_p89j72YY6luAUcu870M8O9Y" }
+                }
+                ,
+                Transport = SocketIOClient.Transport.TransportProtocol.WebSocket
+            });
+
+            socket.OnConnected += (sender, e) =>
+            {
+                Debug.Log("Connected");
+            };
+
+            await socket.ConnectAsync();
+        }
+    }
+}
